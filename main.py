c5835ddeddbca7610db4056ef3d96da3d09be841
Roman <nabroman42@icloud.com>
2021-04-12 21:14:31 +0300

    Add main file with tests

---
+++ main.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    from code.genetic_solution import run_evolution
+
+    image_files = [
+        'source_images/at the hockey - resize.jpg',
+        'source_images/head.jpg']
+
+    for image_file in image_files:
+        try:
+            print(f'Executing: {image_file}')
+            run_evolution(source_image_path=image_file, USE_CUDA=True)
+            print()
+        except KeyboardInterrupt:
+            pass
