c7f574e2b7f4a23aa8f449112b56239d229671dc
Roman <nabroman42@icloud.com>
2021-04-14 00:01:37 +0300

    Change main

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-if __name__ == '__main__':
-    import os
-    from code.evolutionary_solution import run_evolution
+import os
+from code.evolutionary_solution import run_evolution
 
+USE_CUDA = True
+
+if __name__ == '__main__':
     source_img_dir = './source_images'
     for image_file in os.listdir(source_img_dir):
         image_path = os.path.join(source_img_dir, image_file)
         try:
             print(f'Executing: {image_path}')
-            run_evolution(
-                source_image_path=image_path,
-                epoch_count=12,
-                USE_CUDA=True)
-            print()
+            run_evolution(source_image_path=image_path, USE_CUDA=True)
         except KeyboardInterrupt:
             break
+        print()
